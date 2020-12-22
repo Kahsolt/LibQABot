50b3b9f380603d91f41f9d5cc6a299383a07cff5
gotogotogo <1562850087@qq.com>
2020-12-22 13:48:14 +0800

    add asrengine.java to the project

--- app/src/main/java/nju/lib/qabot/mod/ASREngine.java
+++ app/src/main/java/nju/lib/qabot/mod/ASREngine.java
@@ -2,13 +2,32 @@ package nju.lib.qabot.mod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import nju.lib.qabot.util.Requests;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+
+
 public class ASREngine extends Engine {
+
+    static private final String RECOG_URL = String.format("http://%s:%s/recognize", "210.28.134.114", "25102");
+
+
 
     public ASREngine(AppCompatActivity app) { super(app); }
 
     public String recognize(byte[] data) {
         statusShow("audio recognizing");
-        return "I just heard you say nothing";
+        HashMap<String, String> datas = new HashMap<String, String>();
+        datas.put("wav_data", data.toString());
+        JSONObject res = Requests.post(RECOG_URL, datas);
+        boolean ok = res.getBoolean("ok");
+        if(!ok) {
+            statusShow("couldn't get the recognition result, server error?");
+            System.exit(1);
+        }
+        String result = res.getString("result");
+        return result;
     }
 
 }
